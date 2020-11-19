f144685afc9522e1bd0f558bff29b150accdbbe7
Juliana Fogg <julianafogg@gmail.com>
2020-11-18 20:41:12 -0500

    add form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* {
       <ImageLinkForm />
+      {/* {
+
       <FaceRecogniton />} */}
 
     
