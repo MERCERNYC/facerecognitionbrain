f31bd250de5096a61a96f59aecbd609134f1460d
Juliana Fogg <julianafogg@gmail.com>
2020-11-19 11:15:17 -0500

    add state for input

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
+import Rank from './components/Rank/Rank';
 import Logo from './components/Logo/Logo';
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/* {
+import { Component } from 'react';
 
-      <FaceRecogniton />} */}
+const particlesOptions = {
+  
+    particles: {
+      number: {
+      value: 200,
+      density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    }
+  }
 
-    
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+   super();
+   this.state = {
+     input: '',
+   }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Particles className="particles"
+        params={particlesOptions}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm  onInputChange ={this.onInputChange}/>
+        {/* {
+  
+        <FaceRecogniton  />} */}
+  
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+
+
+const Rank = () => {
+    return(
+        <Fragment>
+            <div className='white f3'>
+               {'Juliana, your current rank is ...'}          
+            </div> 
+            
+            <div className='white f1'>
+               {'Rank # 5'}          
+            </div> 
+      </Fragment>  
+    );
+}
+
+export default Rank ;
