212a34b4e46493b65862b4aaf4f3be9d0eb4f243
Juliana Fogg <julianafogg@gmail.com>
2020-11-24 13:57:28 -0500

    add facebox and css

--- src/App.js
+++ src/App.js
@@ -33,8 +33,29 @@ class App extends Component {
    super();
    this.state = {
      input: '',  
-     imageUrl: ''
+     imageUrl: '',
+     box: {}, // a new object state that hold the bounding_box value
    }
+  }
+
+  // this function calculate the facedetect location in the image
+
+  calculateFaceLocation= (data) => {
+   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+   const image = document.getElementById('inputimage');
+   const width = Number(image.width);
+   const height= Number(image.height);
+   return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - clarifaiFace.right_col * width,
+    bottomRow: height - clarifaiFace.bottom_row * height,
+   };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+
   }
 
   onInputChange = (event) => {
@@ -46,19 +67,10 @@ class App extends Component {
     app.models
     .predict(Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-    .then(
-      function (response) {
-        // response data fetch from FACE_DETECT_MODEL 
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        /* data needed from the response data from clarifai API, 
-           note we are just comparing the two for better understanding 
-           would to delete the above console*/  
-      },
-      function (err) {
-        // there was an error
-      }
-    );
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
+  
 
   render() {
 
@@ -72,8 +84,8 @@ class App extends Component {
         <ImageLinkForm  
           onInputChange= {this.onInputChange} 
           onButtonSubmit= {this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        />   
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} /> 
       </div>
     );
 
