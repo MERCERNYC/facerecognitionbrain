cf00c29721f07814fd0b56ab6d84d72b9d8dc750
Juliana Fogg <julianafogg@gmail.com>
2021-01-08 23:59:51 -0500

    update register

--- src/App.js
+++ src/App.js
@@ -38,8 +38,25 @@ class App extends Component {
      imageUrl: '',
      box: {}, // a new object state that hold the bounding_box value
      route: 'signin',
-     isSignedIn : false
+     isSignedIn : false,
+     user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+     }
    }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
   
 
@@ -67,14 +84,30 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input})
     app.models
-    .predict(Clarifai.FACE_DETECT_MODEL,
+    .predict('c0c0ac362b03416da06ab3fa36fb58e3',
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response =>{
+      if (response) {
+        fetch('http://localhost3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}))
+          })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err => console.log(err));
-  }
+}
+
 
   onRouteChange = (route) => {
     if(route === 'signout'){
@@ -82,7 +115,7 @@ class App extends Component {
     } else if ( route === 'home') {
       this.setState({isSignedIn: true})
     }
-    this.setState({route: route});
+    this.setState({route: route})
   }
   
 
@@ -98,17 +131,20 @@ class App extends Component {
         {this.state.route === 'home'
           ? <div>
             <Logo />
-            <Rank />
+            <Rank 
+              name={this.state.user.name} 
+              entries={this.state.user.entries} 
+            />
             <ImageLinkForm  
                 onInputChange= {this.onInputChange} 
-                onButtonSubmit= {this.onButtonSubmit}
+                onPictureSubmit= {this.onPictureSubmit}
             />   
           <FaceRecognition box={box} imageUrl={imageUrl} /> 
             </div>
         : (
             route === 'signin' 
-              ? <Signin onRouteChange= {this.onRouteChange} />
-              : <Register onRouteChange= {this.onRouteChange} />
+              ? <Signin loadUser={this.loadUser} onRouteChange= {this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange= {this.onRouteChange} />
           )    
     }
   
--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
